0b450543f4417ce81df5ed8eecce28834cf58fb6
ev11ccaat <70800641+ev11ccaatt@users.noreply.github.com>
2021-12-13 16:28:05 +0800

    Update cuitautocheck.py

--- cuitautocheck.py
+++ cuitautocheck.py
@@ -5,6 +5,9 @@ url1 = 'http://login.cuit.edu.cn/Login/xLogin/Login.asp'
 url2 ='http://jszx-jxpt.cuit.edu.cn/Jxgl/Xs/netks/sj.asp?jkdk=Y'
 account = sys.argv[1]
 password = sys.argv[2]
+sf1='1'
+sf2='1'
+sf3='1'#填写理由与地点
 r=requests.session()
 header1 ={
         'Host':'login.cuit.edu.cn',
@@ -40,7 +43,8 @@ s=r.get(url3)
 t2=BeautifulSoup(s.text,'lxml')
 s2 = t2.find_all('a')
 s3 = s2[1]
-url4 = 'http://jszx-jxpt.cuit.edu.cn/Jxgl/Xs/netks/editSj.asp?UTp=Xs&Tx=33_1&ObjId='+account+'&Id='+str(s3)[64:69]
+print(str(s3)[64:69])
+url4 = 'http://jszx-jxpt.cuit.edu.cn/Jxgl/Xs/netks/editSj.asp?UTp=Xs&Tx=33_1&ObjId=2020122031&Id='+str(s3)[64:69]
 id = str(s3)[64:69]
 data2 = {
 'RsNum': '3',
@@ -49,13 +53,13 @@ data2 = {
 'canTj': '1',
 'isNeedAns': '0',
 'UTp': 'Xs',
-'ObjId': account,
+'ObjId': 'account',
 'th_1': '21650',
-'wtOR_1': '1%5C%7C%2F%CB%C4%B4%A8%5C%7C%2F%B9%E3%D4%AA%5C%7C%2F%C0%FB%D6%DD%5C%7C%2F1%5C%7C%2F',
+'wtOR_1': '1\\|/%s\\|/%s\\|/%s\\|/\\|/\\|/\\|/\\|/\\|' % (sf1.encode('gb2312'),sf2.encode('gb2312'),sf3.encode('gb2312')),
 'sF21650_1': '1',
-'sF21650_2': '%CB%C4%B4%A8',
-'sF21650_3': '%B9%E3%D4%AA',
-'sF21650_4': '%C0%FB%D6%DD',
+'sF21650_2': sf1.encode('gb2312'),
+'sF21650_3': sf2.encode('gb2312'),
+'sF21650_4': sf3.encode('gb2312'),
 'sF21650_5': '1',
 'sF21650_6': '',
 'sF21650_7': '',
@@ -99,5 +103,8 @@ header2={
 'Accept-Encoding':'gzip, deflate',
 'Accept-Language':'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7'
 }
+#proxies1={'http':'http://127.0.0.1:8080','https':'https://127.0.0.1:8080'}
 r.headers.update(header2)  
+#s=r.post(url='http://jszx-jxpt.cuit.edu.cn/Jxgl/Xs/netks/editSjRs.asp',data=data2,proxies=proxies1)
 s=r.post(url='http://jszx-jxpt.cuit.edu.cn/Jxgl/Xs/netks/editSjRs.asp',data=data2)
+print(s.text)
